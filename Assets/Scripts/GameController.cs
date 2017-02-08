66efc1026498b01412ef3ed2a2659612abf5857b
Gustavo Mazzotti <gustavo@yupistudios.com>
2017-02-08 13:20:55 -0300

    Adicionado GameController da cena.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public int marketTimer;
+	public int marketTimerMax;
+	public float timercount;
+	public int score;
+	public int coints;
+	public Text timerUI;
+	public Text scoreUI;
+	public Text cointsUI;
+
+	void Start () {
+		//CAPTURA DE ELEMENTOS UI
+		timerUI = GameObject.Find ("TimeText").GetComponent<Text> ();
+		scoreUI = GameObject.Find ("ScoreText").GetComponent<Text> ();
+		cointsUI = GameObject.Find ("CashText").GetComponent<Text> ();
+
+		//PARAMETROS INICIAIS
+		this.marketTimer = marketTimerMax;
+		this.coints = 10;
+		this.score = 0;
+
+	}
+
+	void Update () {
+		//CONTROLE TEMPO DO MERCADO
+		timercount += Time.deltaTime;
+		if (timercount > 1) {
+			timercount = 0;
+			marketTimer -= 1;
+		}
+		if (marketTimer <= 0) {
+			marketTimer = marketTimerMax;
+		}
+
+		//ATUALIZA UI
+		timerUI.text = marketTimer.ToString ();
+		scoreUI.text = score.ToString ();
+		cointsUI.text = coints.ToString ();
+
+	}
+}
